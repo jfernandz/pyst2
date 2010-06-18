aae8da965ae4cf46aada8218a4a027ccd392587e
ralf <ralf@01a3061f-1c3a-49da-a2a0-fa5697faa6a0>
2010-06-18 10:02:14 +0000

    - fix setup to include version number and description
    git-svn-id: https://pyst.svn.sourceforge.net/svnroot/pyst/pyst/trunk@7 01a3061f-1c3a-49da-a2a0-fa5697faa6a0

--- setup.py
+++ setup.py
@@ -2,14 +2,33 @@
 
 from distutils.core import setup
 
-setup(name = 'pyst',
-      version = '0.2',
-      description = 'Asterisk related utility modules.',
-      author = 'Karl Putland',
-      author_email = 'kputland@users.sourceforge.net',
-      url = 'http://www.sourceforge.net/projects/pyst/',
-      packages = ['asterisk'],
-      license = 'Python Software Foundation License (agitb), Lesser General Public License',
-      platforms = 'UNIX',
-     )
-      
+try :
+    from asterisk.Version  import VERSION
+except :
+    VERSION = None
+
+description = []
+f = open ('README')
+logo_stripped = False
+for line in f :
+    if not logo_stripped and line.strip () :
+        continue
+    logo_stripped = True
+    description.append (line)
+
+licenses = ( 'Python Software Foundation License (agitb)'
+           , 'GNU Library or Lesser General Public License (LGPL)'
+           )
+
+setup \
+    ( name = 'pyst'
+    , version = VERSION
+    , description = 'A Python Interface to Asterisk'
+    , long_description = ''.join (description)
+    , author = 'Karl Putland'
+    , author_email = 'kputland@users.sourceforge.net'
+    , url = 'http://www.sourceforge.net/projects/pyst/'
+    , packages = ['asterisk']
+    , license = ', '.join (licenses)
+    , platforms = 'UNIX'
+    )
