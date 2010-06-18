05634c2ba77edda8fe5c7bdf3d4b146f203d3b82
ralf <ralf@01a3061f-1c3a-49da-a2a0-fa5697faa6a0>
2010-06-18 10:15:08 +0000

    - add maintainer to setup.py - add credits section and clarification to Todo section
    git-svn-id: https://pyst.svn.sourceforge.net/svnroot/pyst/pyst/trunk@8 01a3061f-1c3a-49da-a2a0-fa5697faa6a0

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup \
     , long_description = ''.join (description)
     , author = 'Karl Putland'
     , author_email = 'kputland@users.sourceforge.net'
+    , maintainer = 'Ralf Schlatterbeck'
+    , maintainer_email = 'rsc@runtux.com'
     , url = 'http://www.sourceforge.net/projects/pyst/'
     , packages = ['asterisk']
     , license = ', '.join (licenses)
