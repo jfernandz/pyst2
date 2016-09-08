1e890057dbe3373de7d0f733be10ff4e412c9da3
poing <brian.lavallee@invite-comm.jp>
2016-09-08 11:11:11 +0900

    Focus on UTF8

--- asterisk/agi.py
+++ asterisk/agi.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-# vim:fenc=utf-8
+# vim: set et sw=4 fenc=utf-8: 
+
 """
 .. module:: agi
    :synopsis: This module contains functions and classes to implment AGI scripts in python. 
